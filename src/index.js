30d12d7d6d42bcf483b630668e30787ad9453801
thenamelessdev <aron.minarovits@gmail.com>
2025-10-23 11:28:14 +0200

    added conf file thing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import { readFileSync } from "fs";
-
 const filename = fileURLToPath(import.meta.url);
 const foldername = dirname(filename);
-
 const app = express();
-
 const TsConfPatch = join(foldername, "/../tsconfig.json");
 const TsConf = readFileSync(TsConfPatch, "utf-8");
 const TsConfJson = JSON.parse(TsConf);
 console.log(TsConfJson);
-
-
-app.get("/", (req: Request, res: Response): void => {
+app.get("/", (req, res) => {
     res.sendFile(foldername + "/public/main.html");
 });
-
-app.get("/tsconf", (req: Request, res: Response): void => {
+app.get("/tsconf", (req, res) => {
     res.json(TsConfJson);
 });
-
 app.listen(8080);
